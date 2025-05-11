ea9fbb8411215a224be7cd54702a590c60b12289
Lokak07 <lokashakthi19.ls@gmail.com>
2025-05-11 11:44:40 +0000

    adding null resource to avoid pipeline continuous running

--- INFRA/eks-install/ebs_addon.tf
+++ INFRA/eks-install/ebs_addon.tf
@@ -1,20 +1,22 @@
-resource "aws_eks_addon" "aws_ebs_csi_driver" {
-  count = 1
-
-  cluster_name  = module.eks.cluster_name
-  addon_name    = "aws-ebs-csi-driver"
-  addon_version = "v1.38.1-eksbuild.2"
-
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  service_account_role_arn = aws_iam_role.ebs_csi_controller_sa.arn
-  depends_on = [aws_iam_role.ebs_csi_controller_sa,
-  aws_iam_role_policy_attachment.ebs_csi_policy]
-
-  preserve = true
-
-  tags = {
-    "eks_addon" = "aws-ebs-csi-driver"
+resource "null_resource" "create_ebs_csi_addon" {
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Creating EBS CSI add-on for cluster: ${module.eks.cluster_name}"
+      aws eks create-addon \
+        --cluster-name ${module.eks.cluster_name} \
+        --addon-name aws-ebs-csi-driver \
+        --addon-version v1.38.1-eksbuild.2 \
+        --service-account-role-arn ${aws_iam_role.ebs_csi_controller_sa.arn} \
+        --resolve-conflicts OVERWRITE || echo "Add-on may already exist or be degraded"
+    EOT
   }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  depends_on = [
+    aws_iam_role.ebs_csi_controller_sa,
+    aws_iam_role_policy_attachment.ebs_csi_policy
+  ]
 }
