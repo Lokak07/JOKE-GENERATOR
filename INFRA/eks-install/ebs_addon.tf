0079fa2689e3ce60005ac1d04547bdf03782576c
Lokak07 <lokashakthi19.ls@gmail.com>
2025-05-11 12:53:36 +0000

    making changes to ebs

--- INFRA/eks-install/ebs_addon.tf
+++ INFRA/eks-install/ebs_addon.tf
@@ -1,22 +1,18 @@
-resource "null_resource" "create_ebs_csi_addon" {
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Creating EBS CSI add-on for cluster: ${module.eks.cluster_name}"
-      aws eks create-addon \
-        --cluster-name ${module.eks.cluster_name} \
-        --addon-name aws-ebs-csi-driver \
-        --addon-version v1.38.1-eksbuild.2 \
-        --service-account-role-arn ${aws_iam_role.ebs_csi_controller_sa.arn} \
-        --resolve-conflicts OVERWRITE || echo "Add-on may already exist or be degraded"
-    EOT
-  }
+resource "aws_eks_addon" "aws_ebs_csi_driver" {
+  count = 1
 
-  triggers = {
-    always_run = "${timestamp()}"
-  }
+  cluster_name  = module.eks.cluster_name
+  addon_name    = "aws-ebs-csi-driver"
+  addon_version = "v1.38.1-eksbuild.2"
 
-  depends_on = [
-    aws_iam_role.ebs_csi_controller_sa,
-    aws_iam_role_policy_attachment.ebs_csi_policy
-  ]
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+
+
+  preserve = true
+
+  tags = {
+    "eks_addon" = "aws-ebs-csi-driver"
+  }
 }
